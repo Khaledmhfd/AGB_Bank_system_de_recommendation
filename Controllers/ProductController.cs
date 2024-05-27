f34e19107178a93904c4c4a7ac5024b9179ec28d
khaled <khaled@DESKTOP-C833PEU>
2024-05-27 16:36:34 +0100

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using AGB_Bank.Data;
+using Microsoft.AspNetCore.Mvc;
+
+/*
+using System.Linq;
+using AGB_Bank.Models.product; // Assurez-vous d'importer votre espace de noms pour les modèles
+*/
+
+
+namespace AGB_Bank.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult card()
+        {
+            var products = _context.carteProduct.ToList();
+            return View(products);
+        }
+
+        public IActionResult Pack()
+        {
+            var products = _context.packProduct.ToList();
+            return View(products);
+        }
+
+        public IActionResult Credit()
+        {
+            var products = _context.creditProduct.ToList();
+            return View(products);
+        }
+
+    }
+
+}
+
+
